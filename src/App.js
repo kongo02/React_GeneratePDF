e1e682f4b275922248aba31355799e3d3d1484ef
Javon Skull <junior.bokanga@gmail.com>
2022-03-31 17:26:17 +0200

    PDF Generation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [setState] = useState({
+		name: "",
+		receiptId: 0,
+		price1: 0,
+		price2: 0,
+	});
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setState((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
+
+	const createAndDownloadPdf = () => {
+		axios
+			.post("/create-pdf", this.state)
+			.then(() => axios.get("fetch-pdf", { responseType: "blob" }))
+			.then((res) => {
+				const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+
+				saveAs(pdfBlob, "newPdf.pdf");
+			});
+	};
+
+	return (
+		<div className="App">
+			<input
+				type="text"
+				placeholder="Name"
+				name="name"
+				onChange={handleChange}
+			/>
+			<input
+				type="number"
+				placeholder="Receipt ID"
+				name="receiptid"
+				onChange={handleChange}
+			/>
+			<input
+				type="number"
+				placeholder="Price 1"
+				name="price1"
+				onChange={handleChange}
+			/>
+			<input
+				type="number"
+				placeholder="Price 2"
+				name="price2"
+				onChange={handleChange}
+			/>
+
+			<button onClick={createAndDownloadPdf}>Download PDF</button>
+		</div>
+	);
+};
 
 export default App;
